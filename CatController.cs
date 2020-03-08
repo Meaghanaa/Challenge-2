d2c9f60eb86fb88e85b14e936a98cdeed76ecb88
Meaghanaa <59702283+Meaghanaa@users.noreply.github.com>
2020-03-08 12:46:56 -0400

    Add files via upload

---
+++ CatController.cs
@@ -0,0 +1,139 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CatController : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+    public float speed;
+    public Text countText;
+    public Text winText;
+    private int count;
+    public Text livesText;
+    private int lives;
+    public AudioSource musicSource;
+    public AudioClip musicClipOne;
+    public AudioClip musicClipTwo;
+    Animator anim;
+    private bool facingRight = true;
+    private bool isOnGround;
+    public Transform groundcheck;
+    public float checkRadius;
+    public LayerMask allGround;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        count = 0;
+        lives = 3;
+        SetCountText();
+        SetlivesText();
+        winText.text = "";
+        musicSource.clip = musicClipOne;
+        musicSource.Play();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float hozMovement = Input.GetAxis("Horizontal");
+        float vertMovement = Input.GetAxis("Vertical");
+        rb2d.AddForce(new Vector2(hozMovement * speed, vertMovement * speed));
+        Debug.Log(hozMovement + "//" + vertMovement + "//" + speed);
+        isOnGround = Physics2D.OverlapCircle(groundcheck.position, checkRadius, allGround);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            anim.SetInteger("State", 1);
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            anim.SetInteger("State", 1);
+        }
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            anim.SetInteger("State", 0);
+        }
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            anim.SetInteger("State", 0);
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            anim.SetInteger("State", 3);
+        }
+        float hozMovement = Input.GetAxis("Horizontal");
+
+        if (facingRight == false && hozMovement > 0)
+        {
+            Flip();
+        }
+        else if (facingRight == true && hozMovement < 0)
+        {
+            Flip();
+        }
+    }
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector2 Scaler = transform.localScale;
+        Scaler.x = Scaler.x * -1;
+        transform.localScale = Scaler;
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Coin")
+        {
+            Destroy(collision.collider.gameObject);
+            count = count + 1;
+            SetCountText();
+        }
+        else if (collision.collider.tag == "Enemy")
+        {
+            Destroy(collision.collider.gameObject);
+            lives = lives - 1;
+            SetlivesText();
+        }
+    }
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count == 8)
+        {
+            Destroy(this);
+            winText.text = "You win! Game created by Meaghan Archer!";
+            musicSource.clip = musicClipTwo;
+            musicSource.Play();
+        }
+        if (count == 4)
+        {
+            transform.position = new Vector2(205.0f, 0.0f);
+            lives = 3;
+            SetlivesText();
+        }
+    }
+    void SetlivesText()
+    {
+        livesText.text = "Lives: " + lives.ToString();
+        if (lives == 0)
+        {
+            Destroy(this);
+            winText.text = "You Lose!";
+        }
+    }
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                rb2d.AddForce(new Vector2(0, 10), ForceMode2D.Impulse);
+            }
+        }
+    }
+}
